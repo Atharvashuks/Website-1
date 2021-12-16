f656a3d14623d185a3299e97d48f05b970750a84
Dheeraj Gupta <dj13042002@gmail.com>
2021-12-16 21:30:55 +0530

    Added aboutus

--- src/app.js
+++ src/app.js
@@ -1,10 +1,95 @@
+// const express = require('express');
+// const path = require('path');
+// require("./db/conn");
+// const hbs = require('hbs');
+
+// const Register = require("./models/register");
+// const { json } = require("express");
+
+
+// const app = express();
+// const port = process.env.PORT || 8000;
+
+// const templatepath = path.join(__dirname,"../templates/views");
+// const partial = path.join(__dirname,"../templates/partials");
+
+
+// app.use('/css',express.static(path.join(__dirname,"../node_modules/bootstrap/dist/css")));
+// app.use('/js',express.static(path.join(__dirname,"../node_modules/bootstrap/dist/js")));
+// app.use('/jq',express.static(path.join(__dirname,"../node_modules/jquery/dist")));
+
+// //routing 
+// //app.get(path,callback)
+
+// app.use(express.json());
+// app.use(express.urlencoded({
+//     extended: false
+// }));
+
+// app.set("view engine","hbs")
+// app.set("views",templatepath);
+// hbs.registerPartials(partial);
+
+// app.get("/",(req,res) =>{
+//     res.render("index");
+// })
+
+// app.get("/aboutus",(req,res) =>{
+//     res.render("aboutus");
+// })
+// app.get("/form",(req,res) =>{
+//     res.render("form");
+// })
+
+// app.get("/login",(req,res) =>{
+//     res.render("login");
+// })
+
+
+
+// //server create
+// app.listen(port, () => {
+//     console.log(`This server is running on port :) ${port} `);
+// })
+
+
+// app.post("/register",async (req,res) => {
+//     try{
+
+//         const password = req.body.pass;
+//         const Cpassword = req.body.Cpass;
+
+//         if(password === Cpassword){
+
+//             const RegI = new Register({
+//                 name:req.body.name,
+//                 email: req.body.email,
+//                 number:req.body.number,
+//                 pass: req.body.pass,
+//                 Cpass:req.body.Cpass,
+//                 course:req.body.course
+//             })
+
+//             const registered = await RegI.save();
+//             res.status(201).render("index");
+
+//         }
+//         else{
+//             res.send("Passwords not matching!!!");
+//         }
+//     }catch(error){
+//         res.status(400).send(error);
+//     }
+// })
+
 const express = require('express');
 const path = require('path');
 require("./db/conn");
 const hbs = require('hbs');
 
-const Register = require("./models/register");
+const Regi = require("./models/register");
 const { json } = require("express");
+const { captureRejectionSymbol } = require('stream');
 
 
 const app = express();
@@ -34,18 +119,23 @@ app.get("/",(req,res) =>{
     res.render("index");
 })
 
-app.get("/aboutus",(req,res) =>{
-    res.render("aboutus");
-})
-app.get("/form",(req,res) =>{
+app.get("/register",(req,res) =>{
     res.render("form");
 })
+
+app.get("/aboutus",(req,res) =>{
+        res.render("aboutus");
+    })
 
 app.get("/login",(req,res) =>{
     res.render("login");
 })
 
-//server create
+app.get("/secret",(req,res) =>{
+    res.render("secret");
+})
+
+//server creaate
 app.listen(port, () => {
     console.log(`This server is running on port :) ${port} `);
 })
@@ -59,7 +149,7 @@ app.post("/register",async (req,res) => {
 
         if(password === Cpassword){
 
-            const RegI = new Register({
+            const RegI = new Regi({
                 name:req.body.name,
                 email: req.body.email,
                 number:req.body.number,
@@ -77,5 +167,30 @@ app.post("/register",async (req,res) => {
         }
     }catch(error){
         res.status(400).send(error);
+    }
+})
+
+//login validation
+
+app.post("/login" , async(req,res) => {
+    try{
+
+        const email = req.body.email;
+        const pass = req.body.pass;
+
+        const userEmail= await Regi.findOne({email: email});
+
+        if(userEmail.pass === pass){
+            res.status(201).render("secret");
+        }
+        else{
+            res.send("Invalid login Details.......");
+        }
+
+        // res.send(userEmail);
+        // console.log(userEmail);
+
+    }catch(error){
+        res.status(400).send("Invalid login Details.......");
     }
 })
